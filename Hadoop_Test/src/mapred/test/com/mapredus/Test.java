5fa5aff16a05e4949c7c662096aa77c2ea4a1973
luoliang861217 <ll@19861217>
2014-12-22 00:15:31 +0800

    ok
    ok

--- Hadoop_Test/src/mapred/test/com/mapredus/Test.java
+++ Hadoop_Test/src/mapred/test/com/mapredus/Test.java
@@ -21,8 +21,8 @@ import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 import com.sun.org.apache.xpath.internal.axes.HasPositionalPredChecker;
 
 public class Test {
-	static final String OUTPUT_DIR = "hdfs://hadoop-master:9000/output/";
-	static final String INPUT_DIR = "hdfs://hadoop-master:9000/input/";
+	static final String OUTPUT_DIR = "hdfs://hadoop-master:9000/mapreduce/output/";
+	static final String INPUT_DIR = "hdfs://hadoop-master:9000/mapreduce/input/test.txt";
 	
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
